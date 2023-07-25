c3e07c979497276b52a237491b2b2690f47f136c
tinhthanh <k40cntt@gmail.com>
2023-07-25 21:21:36 +0700

    init project

---
+++ vetgo-service/src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+
+import { AuthController } from './auth.controller';
+import { Module }  from '@nestjs/common';
+import {AuthService} from "./auth.service";
+
+@Module({
+  controllers: [AuthController],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule {
+
+}
