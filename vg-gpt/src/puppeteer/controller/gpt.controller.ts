a8e0e192fdb54c5f5fd41bdf2df835676b098511
tinhthanh <k40cntt@gmail.com>
2024-09-05 23:06:12 +0700

    fix bug

--- vg-gpt/src/puppeteer/controller/gpt.controller.ts
+++ vg-gpt/src/puppeteer/controller/gpt.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Get, Post, Query, Sse, UseGuards } from "@nestjs/common";
-import { PuppeteerService } from "../service/puppeteer.service";
-import { BehaviorSubject, Observable, Subject } from "rxjs";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Sse,
+  UseGuards,
+} from '@nestjs/common';
+import { PuppeteerService } from '../service/puppeteer.service';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { Semaphore } from 'async-mutex';
-import { Throttle, ThrottlerGuard } from "@nestjs/throttler";
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { v4 as uuidv4 } from 'uuid';
 import { MessageEvent } from '@nestjs/common';
 
@@ -15,7 +23,7 @@ export class GptController {
   private readonly sessions: Map<string, BehaviorSubject<MessageEvent>>;
 
   constructor(private readonly puppeteerService: PuppeteerService) {
-    this.availableProfiles = ["profile1", "profile2", "profile3"];
+    this.availableProfiles = ['profile1', 'profile2', 'profile3'];
     this.semaphore = new Semaphore(this.availableProfiles.length);
     this.profiles = new Set();
     this.sessions = new Map();
@@ -28,33 +36,37 @@ export class GptController {
     }, 0);
   }
 
-  @Post("sse")
+  @Post('sse')
   @Sse()
-  sse(@Body() body: { prompt: string, format?: 'text' | 'html' }): Observable<MessageEvent> {
+  sse(
+    @Body() body: { prompt: string },
+  ): Observable<MessageEvent> {
     const sessionId = uuidv4(); // Tạo một session ID duy nhất cho mỗi yêu cầu
-    const sessionSubject = new BehaviorSubject<MessageEvent>({data:null});
+    const sessionSubject = new BehaviorSubject<MessageEvent>({ data: null });
     this.sessions.set(sessionId, sessionSubject);
     return new Observable<MessageEvent>((ob) => {
       sessionSubject.subscribe(ob);
-      this.executePromptsInternal(sessionId, body.prompt, body.format || 'text')
-      .catch(error => {
-        console.error(error);
-        sessionSubject.error(error);
-      })
-      .finally(() => {
-        sessionSubject.complete();
-        this.sessions.delete(sessionId);
-      });
+      this.executePromptsInternal(sessionId, body.prompt)
+        .catch((error) => {
+          console.error(error);
+          sessionSubject.error(error);
+        })
+        .finally(() => {
+          sessionSubject.complete();
+          this.sessions.delete(sessionId);
+        });
     });
   }
   // hàm này call và đợi kết quả luôn
   @Post()
-  executePrompts(@Body() body: { prompt: string, format?: 'text' | 'html' }): Observable<MessageEvent> {
+  executePrompts(
+    @Body() body: { prompt: string},
+  ): Observable<MessageEvent> {
     const sessionId = uuidv4(); // Tạo một session ID duy nhất cho mỗi yêu cầu
-    const sessionSubject = new BehaviorSubject<MessageEvent>({data:null});
+    const sessionSubject = new BehaviorSubject<MessageEvent>({ data: null });
     this.sessions.set(sessionId, sessionSubject);
-    this.executePromptsInternal(sessionId, body.prompt, body.format || 'text')
-      .catch(error => {
+    this.executePromptsInternal(sessionId, body.prompt)
+      .catch((error) => {
         console.error(error);
         sessionSubject.error(error);
       })
@@ -67,21 +79,26 @@ export class GptController {
 
   @Get('stream')
   @Sse()
-  getPromptsStream(@Query('sessionId') sessionId: string): Observable<MessageEvent> {
+  getPromptsStream(
+    @Query('sessionId') sessionId: string,
+  ): Observable<MessageEvent> {
     const sessionSubject = this.sessions.get(sessionId);
-    console.log('sessionId ' ,sessionId);
+    console.log('sessionId ', sessionId);
     if (!sessionSubject) {
       throw new Error(`Session with ID ${sessionId} not found`);
     }
     return sessionSubject.asObservable();
   }
   // hàm này để thực thi task
-  private async executePromptsInternal(sessionId: string, prompt: string, format: 'text' | 'html' = 'text') {
+  private async executePromptsInternal(
+    sessionId: string,
+    prompt: string
+  ) {
     await this.semaphore.acquire();
     const profile = this.acquireProfile();
     const profileId = profile;
-    const url = "https://chatgpt.com/";
-    const selector = "#prompt-textarea";
+    const url = 'https://chatgpt.com/';
+    const selector = '#prompt-textarea';
     const page = await this.puppeteerService.openOnlyUrl(profileId, url);
 
     const sessionSubject = this.sessions.get(sessionId);
@@ -93,57 +110,99 @@ export class GptController {
       await page.evaluate(() => {
         const popup = document.querySelector('[role="dialog"]');
         if (popup) {
-          popup.querySelectorAll('a').forEach(it => it.click());
+          popup.querySelectorAll('a').forEach((it) => it.click());
         }
       });
 
       await page.waitForSelector(selector);
       await page.focus(selector);
-     const textLen = this.getRandomDelay(5, 10);
-      const firstPart = prompt.slice(0,textLen );
+      const textLen = this.getRandomDelay(5, 10);
+      const firstPart = prompt.slice(0, textLen);
       for (const char of firstPart) {
-        await page.keyboard.type(char, { delay: this.getRandomDelay(100, 300) });
+        await page.keyboard.type(char, {
+          delay: this.getRandomDelay(100, 300),
+        });
       }
 
       const remainingText = prompt.slice(textLen);
-      await page.evaluate((selector, remainingText) => {
-        const input = document.querySelector(selector) as HTMLInputElement;
-        if (input) {
-          input.value += remainingText;
-          const event = new Event('input', { bubbles: true });
-          input.dispatchEvent(event);
-        }
-      }, selector, remainingText);
+      await page.evaluate(
+        (selector, remainingText) => {
+          const input = document.querySelector(selector) as HTMLInputElement;
+          if (input) {
+            input.value += remainingText;
+            const event = new Event('input', { bubbles: true });
+            input.dispatchEvent(event);
+          }
+        },
+        selector,
+        remainingText,
+      );
       console.log(`end step input text`);
-      await  new Promise(resolve => setTimeout(resolve, this.getRandomDelay(500,2000)));
-      await this.puppeteerService.pressKey(profileId, url, "#prompt-textarea", "Enter");
+      await new Promise((resolve) =>
+        setTimeout(resolve, this.getRandomDelay(500, 2000)),
+      );
+      await this.puppeteerService.pressKey(
+        profileId,
+        url,
+        '#prompt-textarea',
+        'Enter',
+      );
       console.log(`end step send key enter`);
 
       const selectorRender = '[data-testid="stop-button"]';
       const selectorMessage = '[data-message-author-role="assistant"]';
       // đợi cho cái button [data-testid="stop-button"] nó hiện lên
-      await new Promise(resolve => setTimeout(resolve, this.getRandomDelay(500,2000)));
+      await page.waitForSelector(selectorRender);
+      // await new Promise(resolve => setTimeout(resolve, this.getRandomDelay(500,2000)));
       console.log(`step đợi ${selectorRender} hiện lên`);
-      await page.exposeFunction('emitMessage', (message: string) => {
-        if(message) {
-          sessionSubject.next({ data: message });
+      await page.exposeFunction('emitMessage', (data: {message: string , type:string, value: string}) => {
+        if (data?.message) {
+          sessionSubject.next({ data: data });
         }
       });
-      await page.evaluate(async (selectorRender, selectorMessage, format) => {
-        const isRender = () => document.querySelector(selectorRender);
-        while (isRender()) {
-          const list = document.querySelectorAll(selectorMessage);
-          if (list.length > 0) {
-            const el: any = list[list.length - 1];
-            if (format === 'html') {
-              (window as any).emitMessage(el.innerHTML);
-            } else {
-              (window as any).emitMessage(el.innerText);
+      await page.evaluate(
+        async (selectorRender, selectorMessage) => {
+          const isRender = () => document.querySelector(selectorRender);
+          while (isRender()) {
+            const list = document.querySelectorAll(selectorMessage);
+            if (list.length > 0) {
+              const el: any = list[list.length - 1];
+               
+              const node = el.querySelector('code');
+              if (node) {
+                const type = el.querySelector('.markdown pre span').innerText;
+                if ((document.body as any).createTextRange) {
+                  const range = (document.body as any).createTextRange();
+                  range.moveToElementText(node);
+                  range.select();
+                  // Lấy nội dung đã chọn
+                  const selectedText = range.text;
+                  console.log('Selected text 1:', selectedText);
+                  (window as any).emitMessage({message:  el.innerHTML , type: type, value: selectedText});
+                } else if (window.getSelection) {
+                  const selection = window.getSelection();
+                  const range = document.createRange();
+                  range.selectNodeContents(node);
+                  selection.removeAllRanges();
+                  selection.addRange(range);
+                  // Lấy nội dung đã chọn
+                  const selectedText = selection.toString();
+                  console.log('Selected text 2:', selectedText);
+                  (window as any).emitMessage({message:  el.innerHTML , type: type, value: selectedText});
+                }
+              } else {
+                (window as any).emitMessage({message:  el.innerHTML , type: "", value: ""});
+              }
+
+                await new Promise((resolve) => setTimeout(resolve, 100));
             }
-            await new Promise(resolve => setTimeout(resolve, 100));
           }
-        }
-      }, selectorRender, selectorMessage, format);
+        },
+        selectorRender,
+        selectorMessage
+      );
+      // nút copy
+      // await page.click('[data-message-author-role="assistant"] .markdown  button:has(svg)');
     } catch (error) {
       console.error(error);
       throw new Error('Failed');
