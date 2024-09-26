69cd88ab85a07d712926993f6fcdd83e27077e51
tinhthanh <k40cntt@gmail.com>
2024-09-27 00:41:37 +0700

    code xam xi

--- zalo-microservices/apps/chrome-profile/src/assets/scripts/send-message.js
+++ zalo-microservices/apps/chrome-profile/src/assets/scripts/send-message.js
@@ -2,7 +2,7 @@
   // return true | false -> true tìm được và mở khung chat len
   // start xử lý input
   const inputStr = sessionStorage.getItem('send-message.js'); // nhận input
-  let input = { msg: '' };
+  let input = { msg: '' , images: []};
   if (inputStr) {
     sessionStorage.removeItem('send-message.js'); // sau khi nhận thì xoá input
     const iObj = JSON.parse(inputStr);
@@ -14,7 +14,18 @@
     console.error('Vui lòng truyền input vào hàm nay !');
     return false;
   }
- await vetgo.se.sendKey("#chat-input-content-id" , input.msg, true );
+  // TODO handle gửi ảnh trước
+  const list = (input?.images || []);
+  if(list.length > 0) {
+    for (let index = 0; index < list.length; index++) {
+      const url = list[index];
+      await vetgo.se.sendKeyImage("#chat-input-content-id" , url);
+    }
+  }
+  // gửi ảnh xong gửi tin
+  if(input?.msg) {
+    await vetgo.se.sendKey("#chat-input-content-id" , input.msg, true );
+  }
   return {status: "DONE"};
   // end xử lý input
 })();
